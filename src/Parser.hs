c4b7c53b5a417b03ad28eef9779ef55d8467bd33
Shockk <shokku.ra@gmail.com>
2014-07-09 13:20:22 +0000

    Rewrote parser functions in lifted State [Token] monad (with ExceptT)

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,27 +1,30 @@
 module Parser where
 
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Except
 import Control.Monad.Trans.State
 import Types.Token (Token)
 import qualified Types.Token as T
 import Types.AST
 import Types.Decl
 
-parsePrimary :: [Token] -> Either String (AST, [Token])
-parsePrimary xs = fail "not implemented"
+parsePrimary :: ExceptT String (State [Token]) AST
+parsePrimary = fail "not implemented"
 
-parseExpr' :: [Token] -> AST -> Integer -> Either String (AST, [Token])
-parseExpr' xs lhs minPrec = fail "not implemented"
+parseExpr' :: Integer -> AST -> ExceptT String (State [Token]) AST
+parseExpr' minPrec lhs = fail "not implemented"
 
-parseExpression :: [Token] -> Either String (AST, [Token])
-parseExpression xs = do
-    (lhs, xss) <- parsePrimary xs
-    parseExpr' xss lhs 0
+parseExpression :: ExceptT String (State [Token]) AST
+parseExpression = parsePrimary >>= parseExpr' 0
 
-parseDeclaration :: [Token] -> Either String (Decl, [Token])
-parseDeclaration (T.Identifier ident : T.Equals : xs) = do
-    (ast, xss) <- parseExpression xs
-    return (Decl ident ast, xss)
-parseDeclaration _ = fail "expected top-level declaration"
+parseDeclaration :: ExceptT String (State [Token]) Decl
+parseDeclaration = do
+    ts <- lift get
+    case ts of
+        T.Identifier ident : T.Equals : xs -> do
+            lift $ modify (drop 2)
+            parseExpression >>= return . (Decl ident)
+        _ -> fail "expected top-level declaration"
 
 parse :: [Token] -> AST
 parse = error "not implemented"
