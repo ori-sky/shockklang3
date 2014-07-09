36620c0b01acfc252f72433cbf3352c720bb664f
Shockk <shokku.ra@gmail.com>
2014-07-09 04:20:23 +0000

    Moved reference code into Main

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
 import Tokenizer (tokenize)
+import Parser (parse)
+
+{-
+
+add = [x y] x + y
+five = 5
+add5 = [x] add x five
+main = add5 2
+
+-}
 
 eval :: String -> String
 eval = show . tokenize
--- src/Types/Token.hs
+++ src/Types/Token.hs
@@ -1,14 +1,5 @@
 module Types.Token where
 
-{-
-
-add = [x y] x + y
-five = 5
-add5 = [x] add x five
-main = add5 2
-
--}
-
 data Token = Equals
            | ArgsBegin
            | ArgsEnd
